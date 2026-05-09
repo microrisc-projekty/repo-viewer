ceb2b15ace4fa566029279678a107cc69a1a7449
janstaffa <jstaffa@janstaffa.cz>
2026-05-09 18:28:56 +0200

    Moved /dist -> /docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/repo-viewer/',
 	plugins: [
 		react(),
 		babel({ presets: [reactCompilerPreset()] }),
